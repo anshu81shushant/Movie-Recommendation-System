c635d071641f86b002b02a2f574958f71b25afc2
anshu81shushant <112415755+anshu81shushant@users.noreply.github.com>
2025-05-01 20:45:45 +0530

    Add files via upload

--- movie_data.py
+++ movie_data.py
@@ -1,1 +1,7 @@
+import gdown
 
+file_id = "1O1bpd5atxVG8OSpjL5o4m_Kdz3VEGHaE"
+url = f"https://drive.google.com/file/d/1O1bpd5atxVG8OSpjL5o4m_Kdz3VEGHaE/view?usp=sharing"
+output = "movie_data.pkl"
+
+gdown.download(url, output, quiet=False)
